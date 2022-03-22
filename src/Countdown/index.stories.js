c42391920287ce9c434d59488948ffa1c7fed9c1
Ludo <ludovic.girard@cdiscount.com>
2022-03-22 11:32:10 +0100

    test: 💍 add Countdown stories

---
+++ src/Countdown/index.stories.js
@@ -0,0 +1,11 @@
+import Countdown from "./";
+
+export default {
+  title: "Countdown",
+  component: Countdown,
+};
+
+export const Default = () => <Countdown />;
+
+export const WithABigStep = () => <Countdown step={50} />;
+export const StartAt10 = () => <Countdown initialCount={10} />;
